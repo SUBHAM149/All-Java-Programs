f584ca14233f16dfa23f047cdff6ffac230baf33
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-01-07 19:41:55 +0530

    Add files via upload

---
+++ MOL1.java
@@ -0,0 +1,15 @@
+//Based on Number of ParaMeters
+void main()
+{
+	IO.println("HELLO MAIN");
+	accept(10,20);
+	accept(10,20,30);
+}
+public void accept(int x,int y)
+{
+	IO.println("Sum of two integer is: "+(x+y));
+}
+public void accept(int x,int y,int z)
+{
+	IO.println("Sum of three  Integer is: "+(x+y+z));
+}
---
+++ MOL3.java
@@ -0,0 +1,13 @@
+//Based on Order Of Parameter
+void main()
+{
+	accept(12,15L);
+}
+public void accept(long l,int x)
+{
+	IO.println("Long & int Combo");
+}
+public void accept(int x,long y)
+{
+	IO.println("Int & Long Combo");
+}
