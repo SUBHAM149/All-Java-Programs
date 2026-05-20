1b2dde20ae90df0201267988454cfc0330ea0860
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-05-20 22:31:30 +0530

    Create Add Elements to ArrayList.java

---
+++ Collection/ArrayList/Add Elements to ArrayList.java
@@ -0,0 +1,15 @@
+1. Add Elements to ArrayList
+import java.util.ArrayList;
+
+public class AddElements {
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Subham");
+        names.add("Rahul");
+        names.add("Ankit");
+
+        System.out.println(names);
+    }
+}
