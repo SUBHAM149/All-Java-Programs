dc7e3cd515375eaea3914172ea4c4753a94583a4
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-06-12 22:24:21 +0530

    Create 3. Remove Element.java

---
+++ Collection/ArrayList/3. Remove Element.java
@@ -0,0 +1,19 @@
+3. Remove Element
+Program
+import java.util.ArrayList;
+
+public class Demo3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        list.remove(1);
+
+        System.out.println(list);
+    }
+}
+Output
+[10, 30]
