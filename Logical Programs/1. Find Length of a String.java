8d56f1d4f31bdbd2a868bc2f20b02a37836e8f8a
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-06-17 22:40:44 +0530

    Create 1. Find Length of a String.java

---
+++ Logical Programs/1. Find Length of a String.java
@@ -0,0 +1,12 @@
+1. Find Length of a String
+class StringLength {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+
+        System.out.println("Length = " + str.length());
+    }
+}
+
+Output:
+
+Length = 16
