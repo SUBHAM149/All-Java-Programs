e955fe06e72d914f0aa7051012f2699def60a604
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-06-17 22:41:48 +0530

    Create 4. Count Vowels in a String.java

---
+++ Logical Programs/4. Count Vowels in a String.java
@@ -0,0 +1,20 @@
+4. Count Vowels in a String
+class CountVowels {
+    public static void main(String[] args) {
+        String str = "Education";
+        int count = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+            char ch = Character.toLowerCase(str.charAt(i));
+
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+                count++;
+        }
+
+        System.out.println("Vowels = " + count);
+    }
+}
+
+Output:
+
+Vowels = 5
