760ffe81fa5a3ca063d061f60407d466c8c1c98a
SUBHAM BEHERA <subhambehera04.03.2005@gmail.com>
2026-06-17 22:42:08 +0530

    Create 5. Count Words in a String.java

---
+++ Logical Programs/5. Count Words in a String.java
@@ -0,0 +1,14 @@
+5. Count Words in a String
+class CountWords {
+    public static void main(String[] args) {
+        String str = "Java is easy";
+
+        String words[] = str.split(" ");
+
+        System.out.println("Words = " + words.length);
+    }
+}
+
+Output:
+
+Words = 3
